b85c6584804c87775de4ea8bbab7b6767b767369
DDiaz <daniel.diaz.0515@gmail.com>
2021-10-26 21:02:06 -0700

    Functions/view; emp,dept,role. Completed

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
-const path = require("path");
+const {} = require("./db");
+const db = require("./db");
+require("console.table");
+//connect seeds sql
+//connect routes folder
 
 const openingQs = [
   {
     type: "list",
-    name: "name",
+    name: "options",
     message: "What would you like to do?",
     choices: [
       "View all departments",
@@ -15,6 +18,7 @@ const openingQs = [
       "Add a role",
       "Add an employee",
       "Update an employee role",
+      "Quit",
     ],
   },
 ];
@@ -76,41 +80,100 @@ const addEmp = [
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
 
 function init() {
-  inquirer.prompt(openingQs).then((answers) => {
-    const response = JSON.stringify(answers);
-    console.log(typeof response);
-    console.log(response);
+  inquirer.prompt(openingQs).then((res) => {
+    //console.log(res.options);
+    switch (res.options) {
+      case "View all employees":
+        viewEmployees();
+        break;
+      case "View all departments":
+        viewDepartments();
+        break;
+      case "View all roles":
+        viewRole();
+        break;
+      case "Add an employee":
+        addEmployee();
+        break;
 
-    //WHEN I choose to View all departments
-    // THEN I am presented with a formatted table showing department names and department ids
-    if (response === "name: View all departments ") {
-      return console.log(response);
-    } else {
-      console.log("unsuccessful ");
+      default:
+        process.exit();
     }
   });
 }
 
+function viewEmployees() {
+  db.findAllEmployees()
+    .then(([data]) => {
+      console.table(data);
+    })
+    .then(() => {
+      init();
+    });
+}
+
+function viewDepartments() {
+  db.findAllDepartments()
+    .then(([data]) => console.table(data))
+    .then(() => init());
+}
+
+function viewRole() {
+  db.findAllRoles()
+    .then(([data]) => console.table(data))
+    .then(() => init());
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "Enter the first name of the employee?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Enter the last name of the employee?",
+      },
+    ])
+    .then((res) => {
+      let firstName = res.first_name;
+      let lastName = res.last_name;
+      // console.log(firstName);
+
+      db.findAllRoles().then(([data]) => {
+        const roleChoices = data.map(({ id, title }) => ({
+          name: title,
+          value: id,
+        }));
+        // var roleId;
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "roleId",
+              message: "what is the employee's role?",
+              choices: roleChoices,
+            },
+          ])
+          .then((res) => {
+            let newEmpRoleId = res.roleId;
+            console.log(newEmpRoleId);
+
+            //             //grab a list of all the employee's  then map the options as choices for manager. make sure the map fuction returns an object that concats the employee name as name, and passes the id as the value. then prompt the user to add the employee manager name. look into unshift function for ability to add a none value. Once that option is chosen create a object that passes the employee information to the addEmployee() then run the init function again
+          });
+      });
+    });
+}
+
 init();
 
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: View all departments, View all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to View all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to View all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// const employee = {
+//   first_name: "Dan",
+//   last_name: "Dia",
+//   role_id: 1,
+// };
 
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+// db.addAnEmployee(employee);
